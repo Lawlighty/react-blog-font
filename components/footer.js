e89899883179228107609c52c496f740871ef4bb
Lawlighty_li <Lawlighty_li@southbaytech.com>
2021-10-23 10:48:17 +0800

    ADD: 加入卜算子

--- components/footer.js
+++ components/footer.js
@@ -1,8 +1,11 @@
 import "../styles/components/footer.css";
+import Script from "react-load-script"
 const Footer = () => (
   <footer className="footer-div">
+    <Script url="//busuanzi.ibruce.info/busuanzi/2.3/busuanzi.pure.mini.js"></Script>
     <div>系统由 React+Node+Ant Desgin+Next.js驱动 </div>
     <div>Lawlighty.com</div>
+    本站总访问量<span id="busuanzi_value_site_pv"></span>次
   </footer>
 );
 
