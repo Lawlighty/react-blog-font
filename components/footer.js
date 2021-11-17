a09b130e4768864271fa7b4b81a217860b216fce
Lawlighty_li <Lawlighty_li@southbaytech.com>
2021-11-17 15:15:32 +0800

    Update footer.js

--- components/footer.js
+++ components/footer.js
@@ -10,7 +10,7 @@ const Footer = () => (
         <a className="hvr-buzz-out"> 浙ICP备2021032851号</a>
       </Link>
     </div>
-    <div>系统由 React+Node+Ant Desgin+Next.js驱动 </div>
+    <div>系统由 React+NestJs+Ant Desgin+NextJs 驱动 </div>
     <div>lawlighty.top</div>
     本站总访问量<span id="busuanzi_value_site_pv"></span>次
   </footer>
